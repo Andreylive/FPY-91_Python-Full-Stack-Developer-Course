7b3946e3089ecf689bc10cfa7f5aafabd17c5d8e
Andrei Pshenichnyi <andrey.pshenichniy@gmail.com>
2023-10-05 20:41:57 +0300

    Print func corrected

--- Homework_OOP.py
+++ Homework_OOP.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -10,15 +12,21 @@ class Student:
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.finished_courses and course in lecturer.courses_attached:
             if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
+                lecturer.grades[course] += grade
             else:
-                lecturer.grades[course] = [grade]
+                lecturer.grades[course] = grade
         else:
             return 'Ошибка'
 
     def __str__(self):
-        return f" Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домашние задания: {self.grades}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}"
-        
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                F"Средняя оценка за домашние задания: {mean(list(self.grades.values()))}\n"
+                f"Rурсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                F"Завершенные курсы: {', '.join(self.finished_courses)}"
+        )
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -27,20 +35,24 @@ class Mentor:
         self.grades = {}
         
 class Lecturer(Mentor):
-    pass
+    def __str__(self):
+     return (f"Имя: {self.name}\n"
+             f"Фамилия: {self.surname}\n"
+             F"Средняя оценка за лекции: {mean(list(self.grades.values()))}\n"
+    )
 
 class Reviewer(Mentor):
         def rate_hw(self, student, course, grade):
             if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
                 if course in student.grades:
-                    student.grades[course] += [grade]
+                    student.grades[course] += grade
                 else:
-                    student.grades[course] = [grade]
+                    student.grades[course] = grade
             else:
                 return 'Ошибка'
         
         def __str__(self):
-            return f" Reviewer - {self.name} {self.surname}"
+            return f"Имя: {self.name}\nФамилия: {self.surname}"
 
 
 # Create students and their sets of courses
@@ -66,8 +78,13 @@ print(best_student.grades)
 # Create a lecturer and her courses and thier raiting
 best_lecturer = Lecturer('Albert', 'Einstein')
 best_lecturer.courses_attached += ['Physics']
+best_lecturer.courses_attached += ['GIT']
 best_student.rate_hw(best_lecturer, 'Physics', 10)
+best_student.rate_hw(best_lecturer, 'GIT', 2)
 print(best_lecturer.courses_attached)
 print(best_lecturer.grades)
 
-# Change pring func behaviour
+# Change print func behaviour
+print(best_student)
+print(cool_reviewer)
+print(best_lecturer)
